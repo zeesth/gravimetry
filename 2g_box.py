e79af8a7b03c8d9a5a7bc65589064b0ebf4b6d15
Vitor <cortes.vitor@hotmail.com>
2024-11-28 12:14:21 -0300

    Added 3d graph.

--- 2g_box.py
+++ 2g_box.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ISIGN = np.array([-1, 1])
-GAMMA = 6.67e-11
+G = 6.67e-11
 
 def distance(length, P):
     obs_x = np.arange(start=0, stop=(length+1), step=10, dtype=float)
-    obs_y = np.zeros(shape=1)
-    obs_z = np.zeros(shape=1)
+    obs_y = np.arange(start=0, stop=(length+1), step=10, dtype=float)
+    obs_x, obs_y = np.meshgrid(obs_x, obs_y)
+    obs_z = np.zeros(1)
 
     rx1 = obs_x - P[0]
     ry1 = obs_y - P[1]
@@ -20,7 +21,7 @@ def distance(length, P):
     y = [ry1, ry2]
     z = [rz1, rz2]
 
-    return x, y, z, obs_x
+    return x, y, z, obs_x, obs_y
 
 def grav_component(rho, x, y, z):
     t_sum = 0
@@ -33,6 +34,9 @@ def grav_component(rho, x, y, z):
                 ijk = ISIGN[i] * ISIGN[j] * ISIGN[k]
 
                 arg1 = np.arctan2((x[i]*y[j]), (z[k]*vertex_r))
+                if np.any(arg1 < 0):
+                    arg1[arg1 < 0] += 2*(np.pi)
+
                 arg2 = vertex_r + y[j]
                 arg3 = vertex_r + x[i]
                 arg2 = np.log(arg2)
@@ -40,32 +44,44 @@ def grav_component(rho, x, y, z):
 
                 t_sum += ijk * (z[k] * arg1 - x[i] * arg2 - y[j] * arg3)
     
-    g = rho * GAMMA * t_sum * 1e8
+    gravz = rho * G * t_sum * 1e8
 
-    return g
+    return gravz
 
 def main():
     size, P1, rho, P2, rho2 = user_input()
 
-    P1x, P1y, P1z, obs1_x = distance(size, P1)
-    P2x, P2y, P2z, obs2_x = distance(size, P2)
+    P1x, P1y, P1z, obs1_x, obs_y = distance(size, P1)
+    P2x, P2y, P2z = distance(size, P2)[:3]
 
     P1grav = grav_component(rho, P1x, P1y, P1z)
     P2grav = grav_component(rho2, P2x, P2y, P2z)
 
     gravsum = P1grav + P2grav
-    plots(obs1_x, gravsum, P1grav, P2grav, size)
+    plots(obs1_x, obs_y, gravsum, P1grav, P2grav, size)
 
-def plots(obs_x, grav, grav1, grav2, size):
-    plt.plot(obs_x/1000, grav, label="Sum")
-    plt.plot(obs_x/1000, grav1, label="Prism 1")
-    plt.plot(obs_x/1000, grav2, label="Prism 2")
-    plt.xlabel("x (km)")
-    plt.ylabel("Gz (mGal)")
-    plt.title("Gz in function of x")
-    plt.legend()
-    plt.xlim(0, size/1000)
-    plt.grid()
+def plots(obs_x, obs_y, grav, grav1, grav2, size):
+    fig = plt.figure(figsize=[12.8, 4.8])
+    y_filter = np.where(obs_y==0)[0][0]
+
+    trid = fig.add_subplot(1, 2, 1, projection="3d",
+    xlabel="x (km)",
+    ylabel="y (km)",
+    zlabel="Gz (mGal)",
+    title="Gz in function of x")
+    trid.plot_surface(obs_x/1000, obs_y/1000, grav, cmap="viridis")
+    trid.view_init(azim=230)
+
+    twod = fig.add_subplot(1, 2, 2,
+    xlabel="x (km)",
+    ylabel="Gz (mGal)",
+    title="Gz in function of x",)
+    twod.plot(obs_x[0]/1000, grav[y_filter, :], label="Sum")
+    twod.plot(obs_x[0]/1000, grav1[y_filter, :], label="Prism 1")
+    twod.plot(obs_x[0]/1000, grav2[y_filter, :], label="Prism 2")
+    twod.grid()
+    twod.set_xlim(0, size/1000)
+    twod.legend()
     plt.show()
 
 def user_input():
