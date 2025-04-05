717b816c32ddcddc065f13169b3f82e0253b7255
Vitor <cortes.vitor@hotmail.com>
2025-04-05 20:19:24 -0300

    Added a body plot and foundations for 3d analysis.

--- 2d_basin.py
+++ 2d_basin.py
@@ -1,19 +1,19 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import numpy as np
 
 ISIGN = np.array([-1, 1])
 G = 6.67e-11
 
-def distance(obs_x, params):
-    obs_y = np.zeros_like(obs_x)
+def distance(obs_x, obs_y, params):
     obs_z = np.zeros_like(obs_x)
-    
+
     ## Calculates the distance between the observator and each vertex
     rx1 = obs_x - params[0]
     ry1 = obs_y - 1
-    rz1 = obs_z - 1
+    rz1 = obs_z - 0
     rx2 = obs_x - params[1]
-    ry2 = obs_y - 1000
+    ry2 = obs_y - 1
     rz2 = obs_z - params[2]
 
     x = [rx1, rx2]
@@ -53,34 +53,37 @@ def main():
     size = 1000 * float(input("Grid size (km): "))
     bodies = int(input("How many prisms do you want to plot? "))
 
-    obs_x, param_t, rho_t = init(bodies, size)
+    obs_x, obs_y, param_t, rho_t = init(bodies, size)
 
-    grav_t = multi(bodies, param_t, rho_t, obs_x)
-
+    grav_t = multi(bodies, param_t, rho_t, obs_x, obs_y)
     grav_sum = sum(grav_t)
-    plots(bodies, obs_x, grav_sum, grav_t)
+
+    plots(bodies, size, param_t, obs_x, obs_y, grav_sum, grav_t)
 
 def init(bodies, size):
     param_t = []
     rho_t = []
 
     for n in range(bodies):
-        temp_xyz, temp_rho = get_params(n+1)
+        temp_xyz, temp_rho = user_input(n+1, size, bodies)
         param_t.append(temp_xyz)
         rho_t.append(temp_rho)
-    
+
     obs_x = np.arange(start=0, stop=(size+1), step=10, dtype=float)
+    obs_y = np.arange(start=0, stop=(1001), step=10, dtype=float)
+    obs_x, obs_y = np.meshgrid(obs_x, obs_y)
+    param_t = np.array(param_t)
 
-    return obs_x, param_t, rho_t
+    return obs_x, obs_y, param_t, rho_t
 
-def multi(bodies, param_t, rho_t, obs_x):
+def multi(bodies, param_t, rho_t, obs_x, obs_y):
     param_x = []
     param_y = []
     param_z = []
     grav_t = []
 
     for n in range(bodies):
-        temp_x, temp_y, temp_z = distance(obs_x, param_t[n])
+        temp_x, temp_y, temp_z = distance(obs_x, obs_y, param_t[n])
         param_x.append(temp_x)
         param_y.append(temp_y)
         param_z.append(temp_z)
@@ -88,40 +91,61 @@ def multi(bodies, param_t, rho_t, obs_x):
     
     return grav_t
 
-def plots(bodies, obs_x, grav_sum, grav_t):
+def plots(bodies, size, param_t, obs_x, obs_y, grav_sum, grav_t):
+    fig = plt.figure(figsize=[13, 10])
+    y_filter = np.where(obs_y==0)[0][0]
+    prop = size/(bodies*1000)
 
-    fig = plt.figure(figsize=[12.8, 4.8])
-
-    total = fig.add_subplot(1, 2, 1,
+    ## Plots total Gz
+    total = fig.add_subplot(2, 2, 1,
     xlabel="x (km)",
     ylabel="Gz (mGal)",
-    title="Total Gz")
-    total.plot(obs_x/1000, grav_sum)
+    title="Total Gz in function of x")
+    total.plot(obs_x[0]/1000, grav_sum[y_filter, :], label="Sum", color="k")
     total.grid()
-    total.set_xlim(0, obs_x[-1]/1000)
+    total.set_xlim(0, obs_x[0][-1]/1000)
 
-    indiv = fig.add_subplot(1, 2, 2,
+    ## Plots each prism's Gz
+    solo = fig.add_subplot(2, 2, 2,
     xlabel="x (km)",
     ylabel="Gz (mGal)",
-    title="Individual Gz")
+    title="Gz in function of x")
     for n in range(bodies):
-        indiv.plot(obs_x/1000, grav_t[n], label=f"Prism {n+1}")
-    
-    indiv.grid()
-    indiv.set_xlim(0, obs_x[-1]/1000)
-    indiv.legend()
+        solo.plot(obs_x[0]/1000, grav_t[n][y_filter, :], label=f"Prism {n+1}")
+    solo.grid()
+    solo.set_xlim(0, obs_x[0][-1]/1000)
+
+    ## Plots body position
+    body1 = fig.add_subplot(2, 2, 3,
+    xlabel = "x (km)",
+    ylabel = "z (km)")
+    for i in range(bodies):
+        body1.add_patch(Rectangle(((i) * prop, 0), prop, param_t[i][2]/1000, fc="y", ec="k"))
+    body1.set_xlim(0, obs_x[0][-1]/1000)
+    body1.set_ylim(0, (np.max(param_t[:, 2]))/800)
+    body1.invert_yaxis()
+    body1.set_aspect('equal')
+
+    body2 = fig.add_subplot(2, 2, 4,
+    xlabel = "x (km)",
+    ylabel = "z (km)")
+    for i in range(bodies):
+        body2.add_patch(Rectangle(((i) * prop, 0), prop, param_t[i][2]/1000, fc="y", ec="k"))
+    body2.set_xlim(0, obs_x[0][-1]/1000)
+    body2.set_ylim(0, (np.max(param_t[:, 2]))/800)
+    body2.invert_yaxis()
+    body2.set_aspect('equal')
     
     plt.show()
 
-
-def get_params(n=1):
-    x1 = (n-1) * 2000
-    x2 = x1 + 2000
+def user_input(n=1, size=3, bodies=10):
+    prop = size/bodies
+    x1 = (n-1) * prop
+    x2 = x1 + prop
     z = 1000 * float(input(f"Prism number {n} depth (km): "))
-    rho = float(input(f"Prism number {n} density (kg/m³): "))
+    rho = 2
 
     params = [x1, x2, z]
-    
     return params, rho
 
 main()
